ef78e7cc204fdd2349a9fa8f4d383aabcc63b639
chenlb <chenlbphoto@163.com>
2024-11-28 07:22:17 +0800

    feat: 增加 dashscope 的 retry 功能；retry 统计信息也记录到 langfuse。

--- langfarm/hooks/dashscope/generation.py
+++ langfarm/hooks/dashscope/generation.py
@@ -1,6 +1,9 @@
-from typing import Any, List, Union, Dict, Generator
+import json
+import logging
+from typing import Any, List, Union, Dict, Generator, Callable
 
 from langfuse.decorators import langfuse_context
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type, before_sleep_log
 
 try:
     import dashscope
@@ -18,6 +21,34 @@ except ImportError:
     GenerationResponse = None
 
 
+logger = logging.getLogger(__name__)
+
+
+def _create_retry_decorator(max_retries: int) -> Callable[[Any], Any]:
+    min_seconds = 1
+    max_seconds = 4
+    # Wait 2^x * 1 second between each retry starting with
+    # 4 seconds, then up to 10 seconds, then 10 seconds afterward
+    return retry(
+        reraise=True,
+        stop=stop_after_attempt(max_retries),
+        wait=wait_exponential(multiplier=1, min=min_seconds, max=max_seconds),
+        retry=(retry_if_exception_type(RetryGenerationException)),
+        before_sleep=before_sleep_log(logger, logging.WARNING),
+    )
+
+
+class FailedGenerationException(Exception):
+
+    def __init__(self, message: str, response: GenerationResponse):
+        self.message = message
+        self.response: GenerationResponse = response
+
+
+class RetryGenerationException(FailedGenerationException):
+    pass
+
+
 class Generation(TongyiGeneration):
 
     @classmethod
@@ -30,7 +61,7 @@ class Generation(TongyiGeneration):
         return output
 
     @classmethod
-    def _up_current_observation(cls, model: str, input_query: str, output: str, usage: dict):
+    def _up_generation_observation(cls, model: str, input_query: str, output: str, usage: dict):
         # 解释 token usage
         langfuse_context.update_current_observation(
             name="Dashscope-generation", model=model
@@ -43,12 +74,12 @@ class Generation(TongyiGeneration):
         )
 
     @classmethod
-    def _up_general_observation(cls, input_query: Any, model: str, result_format: str, response: GenerationResponse):
+    def _up_general_generation_observation(cls, input_query: Any, model: str, result_format: str, response: GenerationResponse):
         output = cls.response_to_output(result_format, response)
-        cls._up_current_observation(model, input_query, output, response.usage)
+        cls._up_generation_observation(model, input_query, output, response.usage)
 
     @classmethod
-    def _up_stream_observation(
+    def _up_stream_generation_observation(
             cls, input_query: Any, model: str, result_format: str
             , response: Generator[GenerationResponse, None, None], incremental_output: bool = False
     ) -> Generator[GenerationResponse, None, None]:
@@ -73,13 +104,99 @@ class Generation(TongyiGeneration):
             last_usage = {"input_tokens": 0, "output_tokens": 0}
 
         # 解释 token usage
-        cls._up_current_observation(model, input_query, output, last_usage)
+        cls._up_generation_observation(model, input_query, output, last_usage)
+
+    @classmethod
+    def _up_error_observation(cls, input_query: Any, model: str, response: GenerationResponse):
+        level = 'WARNING' if response.status_code == 429 else 'ERROR'
+        langfuse_context.update_current_observation(
+            name="Dashscope-generation", model=model
+            , input=input_query, output=None
+            , level=level
+            , status_message=json.dumps({
+                'status_code': response.status_code
+                , 'err_code': response.code
+                , 'err_msg': response.message
+            }, ensure_ascii=False)
+        )
+
+    @classmethod
+    def _do_call(cls, model: str, prompt: Any = None, history: list = None, api_key: str = None,
+             messages: List[Message] = None, plugins: Union[str, Dict[str, Any]] = None, workspace: str = None,
+             **kwargs) -> Union[GenerationResponse, Generator[GenerationResponse, None, None]]:
+
+        response = super().call(model, prompt, history, api_key, messages, plugins, workspace, **kwargs)
+
+        return response
+
+    @classmethod
+    def check_response(cls, resp: Any) -> Any:
+        """Check the response from the completion call."""
+        if resp["status_code"] == 200:
+            return resp
+        elif resp["status_code"] in [400, 401]:
+            raise FailedGenerationException(
+                f"status_code: {resp['status_code']} \n "
+                f"code: {resp['code']} \n message: {resp['message']}"
+                , response=resp
+            )
+        else:
+            raise RetryGenerationException(
+                f"HTTP error occurred: status_code: {resp['status_code']} \n "
+                f"code: {resp['code']} \n message: {resp['message']}",
+                response=resp,
+            )
+
+    @classmethod
+    def _up_retry_stat_to_observation(cls, max_retries: int, retry_stat: dict):
+        if 'attempt_number' in retry_stat:
+            retry_cnt = retry_stat['attempt_number']
+            if retry_cnt > 1:
+                langfuse_context.update_current_observation(
+                    metadata={
+                        'retry_cnt': retry_cnt - 1
+                        , 'idle_second': retry_stat['idle_for']
+                        , 'max_retries': max_retries
+                    }
+                )
+
+    @classmethod
+    def generate_with_retry(cls, max_retries: int, **kwargs: Any) -> GenerationResponse:
+        """Use tenacity to retry the completion call."""
+        retry_decorator = _create_retry_decorator(max_retries)
+
+        @retry_decorator
+        def _generate_with_retry(**_kwargs: Any) -> GenerationResponse:
+            resp = cls._do_call(**_kwargs)
+            return cls.check_response(resp)
+
+        try:
+            response = _generate_with_retry(**kwargs)
+        except FailedGenerationException as e:
+            response = e.response
+
+        # 记录重试信息
+        retry_stat = _generate_with_retry.statistics
+        cls._up_retry_stat_to_observation(max_retries, retry_stat)
+        return response
+
+    @classmethod
+    def stream_generate_with_retry(cls, max_retries: int, **kwargs: Any) -> Generator[GenerationResponse, None, None]:
+        """Use tenacity to retry the completion call."""
+        retry_decorator = _create_retry_decorator(max_retries)
+
+        @retry_decorator
+        def _stream_generate_with_retry(**_kwargs: Any) -> Generator[GenerationResponse, None, None]:
+            responses = cls._do_call(**_kwargs)
+            for resp in responses:
+                yield cls.check_response(resp)
+
+        return _stream_generate_with_retry(**kwargs)
 
     @classmethod
     def call(cls, model: str, prompt: Any = None, history: list = None, api_key: str = None,
              messages: List[Message] = None, plugins: Union[str, Dict[str, Any]] = None, workspace: str = None,
              **kwargs) -> Union[GenerationResponse, Generator[GenerationResponse, None, None]]:
-        response = super().call(model, prompt, history, api_key, messages, plugins, workspace, **kwargs)
 
         # input
         input_query = None
@@ -91,10 +208,28 @@ class Generation(TongyiGeneration):
         # output
         result_format = kwargs.get("result_format")
         incremental_output = kwargs.get("incremental_output", False)
-        if isinstance(response, Generator):
-            return cls._up_stream_observation(input_query, model, result_format, response, incremental_output)
+
+        # is stream
+        stream = kwargs.get("stream", False)
+        max_retries = kwargs.pop("max_retries", 10)
+        if stream:
+            response = cls.stream_generate_with_retry(
+                max_retries
+                , model=model, prompt=prompt, history=history, api_key=api_key, messages=messages
+                , plugins=plugins, workspace=workspace
+                , **kwargs
+            )
+            return cls._up_stream_generation_observation(input_query, model, result_format, response, incremental_output)
         else:
+            response = cls.generate_with_retry(
+                max_retries
+                , model=model, prompt=prompt, history=history, api_key=api_key, messages=messages
+                , plugins=plugins, workspace=workspace
+                , **kwargs
+            )
             if response.status_code == 200:
-                cls._up_general_observation(input_query, model, result_format, response)
+                cls._up_general_generation_observation(input_query, model, result_format, response)
+            else:
+                cls._up_error_observation(input_query, model, response)
 
         return response
---
+++ tests/test_hook_dashscope_retry.py
@@ -0,0 +1,172 @@
+import logging
+import os
+import unittest
+from typing import Any, List, Union, Dict, Generator
+
+from dashscope.api_entities.dashscope_response import Message, GenerationResponse, GenerationUsage, GenerationOutput
+from langfuse.decorators import observe, langfuse_context
+from tenacity import stop_after_attempt, retry, wait_fixed, before_sleep_log, wait_random
+
+from langfarm.hooks.dashscope import Generation
+from tests.base import BaseTestCase
+
+logger = logging.getLogger(__name__)
+
+
+class MyException(Exception):
+    pass
+
+
+class MockGeneration(Generation):
+
+    max_fail_cnt = 3
+    fail_cnt = 0
+
+    @classmethod
+    def _reset_fail_cnt(cls):
+        cls.fail_cnt = 0
+
+    @classmethod
+    def _do_call(cls, model: str, prompt: Any = None, history: list = None, api_key: str = None,
+                 messages: List[Message] = None, plugins: Union[str, Dict[str, Any]] = None, workspace: str = None,
+                 **kwargs) -> Union[GenerationResponse, Generator[GenerationResponse, None, None]]:
+
+        cls.fail_cnt += 1
+        if cls.fail_cnt >= cls.max_fail_cnt:
+            response = GenerationResponse(
+                status_code=200
+                , usage=GenerationUsage(input_tokens=20, output_tokens=5)
+                , output=GenerationOutput(text='mock for success', finish_reason='done')
+            )
+        else:
+            response = GenerationResponse(
+                status_code=429, code='RateLimit', message='mock test rate-limit'
+                , usage=GenerationUsage(input_tokens=20, output_tokens=5)
+                , output=GenerationOutput(text='mock for rate-limit', finish_reason='done')
+            )
+
+        return response
+
+
+class MockErrorGeneration(Generation):
+
+    @classmethod
+    def _do_call(cls, model: str, prompt: Any = None, history: list = None, api_key: str = None,
+                 messages: List[Message] = None, plugins: Union[str, Dict[str, Any]] = None, workspace: str = None,
+                 **kwargs) -> Union[GenerationResponse, Generator[GenerationResponse, None, None]]:
+
+        response = GenerationResponse(
+            status_code=400, code='BadRequest', message='mock test bad request'
+        )
+        return response
+
+class MyTestCase(BaseTestCase):
+
+    def setUp(self):
+        self.cnt = 0
+        MockGeneration._reset_fail_cnt()
+
+    @retry(stop=stop_after_attempt(4), wait=wait_fixed(1)+wait_random(0, 2), before_sleep=before_sleep_log(logger, logging.WARNING))
+    def raise_my_exception(self):
+        self.cnt += 1
+        if self.cnt >= 3:
+            logger.info("cnt = %s", self.cnt)
+        else:
+            raise MyException(f"Fail cnt={self.cnt}")
+
+    def test_demo_retry(self):
+        logger.info("start demo_retry")
+        try:
+            self.raise_my_exception()
+        except Exception as e:
+            logger.error("error %s", e)
+
+        retry_stat = self.raise_my_exception.statistics
+        logger.info(retry_stat)
+        assert retry_stat['attempt_number'] == 3
+        assert retry_stat['idle_for'] >= 2
+
+    @observe(as_type="generation")
+    def tongyi_gen(self, query: str, model_name: str, max_retries: int = 10) -> str:
+        response = MockGeneration.call(
+            api_key=os.getenv('DASHSCOPE_API_KEY'),
+            model=model_name,
+            prompt=query,
+            max_retries=max_retries,
+        )
+        if response.status_code == 200:
+            if hasattr(response.output, "text"):
+                return response.output.text
+            else:
+                # result_format="message"
+                return response.output.choices[0].message.content
+        else:
+            return ''
+
+    @observe()
+    def test_dashscope_retry(self):
+        MockGeneration.max_fail_cnt = 3
+        logger.info("start dashscope_retry")
+        query = 'start dashscope retry'
+        output = self.tongyi_gen(query, 'qwen-plus')
+        logger.info("output=%s", output)
+        langfuse_context.update_current_trace(
+            input=query, output=output
+        )
+        langfuse_context.flush()
+
+    @observe()
+    def test_dashscope_no_retry(self):
+        MockGeneration.max_fail_cnt = 1
+        logger.info("start dashscope_retry")
+        query = 'start dashscope retry'
+        output = self.tongyi_gen(query, 'qwen-plus')
+        logger.info("output=%s", output)
+        langfuse_context.update_current_trace(
+            input=query, output=output
+        )
+        langfuse_context.flush()
+
+    @observe()
+    def test_dashscope_over_max_retry(self):
+        MockGeneration.max_fail_cnt = 4
+        logger.info("start dashscope_retry")
+        query = 'start dashscope retry'
+        output = self.tongyi_gen(query, 'qwen-plus', 3)
+        logger.info("output=%s", output)
+        langfuse_context.update_current_trace(
+            input=query, output=output
+        )
+        langfuse_context.flush()
+
+    @observe(as_type="generation")
+    def tongyi_fail_gen(self, query: str, model_name: str, max_retries: int = 10) -> str:
+        response = MockErrorGeneration.call(
+            api_key=os.getenv('DASHSCOPE_API_KEY'),
+            model=model_name,
+            prompt=query,
+            max_retries=max_retries,
+        )
+        if response.status_code == 200:
+            if hasattr(response.output, "text"):
+                return response.output.text
+            else:
+                # result_format="message"
+                return response.output.choices[0].message.content
+        else:
+            return ''
+
+    @observe()
+    def test_dashscope_bad_request(self):
+        logger.info("start dashscope_retry")
+        query = 'start dashscope retry'
+        output = self.tongyi_fail_gen(query, 'qwen-plus', 3)
+        logger.info("output=%s", output)
+        langfuse_context.update_current_trace(
+            input=query, output=output
+        )
+        langfuse_context.flush()
+
+
+if __name__ == '__main__':
+    unittest.main()
